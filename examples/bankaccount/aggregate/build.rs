e7794a9082b55c05213072ac179360d56cf64b24
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2023-06-30 10:33:24 -0400

    Removing redundant markdown generation
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- examples/bankaccount/aggregate/build.rs
+++ examples/bankaccount/aggregate/build.rs
@@ -3,10 +3,7 @@ use concordance_gen::EntityType;
 const CONFIG: &str = "./codegen.toml";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    weld_codegen::rust_build(CONFIG)?;
-
-    // Generate markdown docs
-    concordance_gen::generate_doc("../bankaccount-model.ttl".into(), "../model/docs".into())?;
+    weld_codegen::rust_build(CONFIG)?;    
 
     // Generate the relevant traits
     concordance_gen::generate_system_traits("../bankaccount-model.ttl".into(), "./src".into())?;
--- examples/bankaccount/process_manager/src/genimpl.rs
+++ examples/bankaccount/process_manager/src/genimpl.rs
@@ -3,7 +3,7 @@ use crate::eventsourcing::*;
 
 use crate::*;
 
-use wasmcloud_interface_logging::{debug, error};
+use wasmcloud_interface_logging::{error, debug};
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, ProcessManagerService)]
@@ -11,59 +11,55 @@ pub(crate) struct BankaccountProcessManagerImpl {}
 
 #[async_trait]
 impl ProcessManagerService for BankaccountProcessManagerImpl {
-    async fn handle_event(
-        &self,
-        _ctx: &Context,
-        arg: &EventWithState,
-    ) -> RpcResult<ProcessManagerAck> {
+    
+    async fn handle_event(&self, _ctx: &Context, arg: &EventWithState) -> RpcResult<ProcessManagerAck> {
         let state: Option<BankaccountProcessManagerState> = arg
-            .state
-            .clone()
-            .map(|bytes| deserialize_json(&bytes).unwrap_or_default());
+         .state
+         .clone()
+         .map(|bytes| deserialize_json(&bytes).unwrap_or_default());
 
         Ok(match arg.event.event_type.as_str() {
             WireTransferRequested::TYPE => {
                 BankaccountProcessManager::handle_wire_transfer_requested(
                     self,
-                    deserialize_json(&arg.event.payload)?,
-                    state,
-                )?
-            }
-            WireFundsReserved::TYPE => BankaccountProcessManager::handle_wire_funds_reserved(
-                self,
-                deserialize_json(&arg.event.payload)?,
-                state,
-            )?,
+                    deserialize_json(&arg.event.payload)?,                    
+                    state)?
+                },
+            WireFundsReserved::TYPE => {
+                BankaccountProcessManager::handle_wire_funds_reserved(
+                    self,
+                    deserialize_json(&arg.event.payload)?,                    
+                    state)?
+                },
             InterbankTransferCompleted::TYPE => {
                 BankaccountProcessManager::handle_interbank_transfer_completed(
                     self,
-                    deserialize_json(&arg.event.payload)?,
-                    state,
-                )?
-            }
+                    deserialize_json(&arg.event.payload)?,                    
+                    state)?
+                },
             InterbankTransferFailed::TYPE => {
                 BankaccountProcessManager::handle_interbank_transfer_failed(
                     self,
-                    deserialize_json(&arg.event.payload)?,
-                    state,
-                )?
-            }
+                    deserialize_json(&arg.event.payload)?,                    
+                    state)?
+                },
             InterbankTransferInitiated::TYPE => {
                 BankaccountProcessManager::handle_interbank_transfer_initiated(
                     self,
-                    deserialize_json(&arg.event.payload)?,
-                    state,
-                )?
-            }
-            e => {
+                    deserialize_json(&arg.event.payload)?,                    
+                    state)?
+                },
+            e =>  {
                 debug!("Unexpected event received '{e}'. Acking and moving on - Is interest configured properly??");
                 ProcessManagerAck::ok(state, vec![])
             }
-        })
+        })        
     }
 }
 
-fn deserialize_json<'de, T: Deserialize<'de>>(buf: &'de [u8]) -> RpcResult<T> {
+fn deserialize_json<'de, T: Deserialize<'de>>(
+    buf: &'de [u8],
+) -> RpcResult<T> {
     serde_json::from_slice(buf).map_err(|e| format!("Deserialization failure: {e:?}").into())
 }
 
@@ -75,7 +71,7 @@ impl ProcessManagerAck {
     pub fn ok(state: Option<impl Serialize>, cmds: CommandList) -> Self {
         Self {
             state: state.map(|s| serialize_json(&s).unwrap_or_default()),
-            commands: cmds,
+            commands: cmds
         }
     }
 }
