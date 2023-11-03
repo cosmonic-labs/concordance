d2d964f604b9dfcb176f67fa5efbf9da84648588
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2023-11-03 10:03:52 -0400

    feat(bankaccount): add wadm manifest
    * adding wadm
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    
    * Update bank_wadm.yaml
    
    * Adding redis to bankaccount wadm
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    
    * Cleaning up scripts
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    
    * Update examples/bankaccount/bank_wadm.yaml
    
    Co-authored-by: Brooks Townsend <brooksmtownsend@gmail.com>
    
    ---------
    
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>
    Co-authored-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- examples/bankaccount/scripts/README.md
+++ examples/bankaccount/scripts/README.md
@@ -1,2 +1,4 @@
 # ⚠️ NOTE
 These scripts have not yet been updated to the new model. They will be updated soon.
+
+See the [bank_wadm.yaml](../bank_wadm.yaml) file for the most up to date configuration and startup.
--- examples/bankaccount/scripts/interbank_1.sh
+++
@@ -1,9 +0,0 @@
-# uncomment if you want to start fresh
-# nats stream purge CC_EVENTS -f
-# nats stream purge CC_COMMANDS -f
-
-nats req cc.commands.bankaccount "`cat ibt_create_account_cmd.json | jq -c`"
-nats req cc.commands.bankaccount "`cat ibt_request_wire_xfer.json | jq -c`"
-
-nats kv get CC_STATE agg.bankaccount.834597DRML130 --raw | jq
-nats kv get CC_STATE pm.interbankxfer.9c0562bb-6657-4918-ad21-bec63f38bc10 --raw | jq
